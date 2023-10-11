d77ea0ad076757c0a5cc3a4530597c394575c5b0
Bikatr7 <Tetralon07@gmail.com>
2023-10-10 20:27:42 -0600

    Here i'll just unfuck it afterwards.
    none -> gradle

--- KusariKey.bat
+++
@@ -1,4 +0,0 @@
-cd C:\Users\Tetra\Documents\Repositories\KusariKey\src
-javac KusariKey.java Login.java CoreModule/Core.java -d ../bin
-cd C:\Users\Tetra\Documents\Repositories\KusariKey\bin
-java -cp ../bin KusariKey
--- README.md
+++
@@ -1,3 +0,0 @@
-**KusariKey**
-------------------------------------------
-DO NOT USE THIS, IT'LL ONLY RUN ON MY COMPUTER AND IS NOT SECURE AT ALL.
