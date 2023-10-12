719b3773dd45171d2eb68a92cf88f426f5d5c203
Bikatr7 <Tetralon07@gmail.com>
2023-10-12 14:07:22 -0600

    further prep for logger

--- src/KusariKey.java
+++ src/KusariKey.java
@@ -7,6 +7,7 @@ import java.io.*;
 import Entities.Login;
 import Handlers.*;
 
+
 public class KusariKey 
 {
     public static void main(String[] args) throws Exception 
--- src/Modules/logger.java
+++ src/Modules/logger.java
@@ -1,6 +1,48 @@
 package Modules;
 
+import java.nio.file.Path;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+//-------------------start-of-logger---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 public class logger 
 {
-    
+
+    private String batch;
+    private Path logPath;
+
+//-------------------start-of-logger()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ 
+    public logger(Path logPath)
+    {
+        this.logPath = logPath;
+        this.batch = "";
+        
+    }
+
+//-------------------start-of-getTimestamp()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    /**
+     * Gets the current timestamp for an action to be logged
+     * @param None
+     * @return formattedDateTime - String
+     */
+
+    private String getTimestamp()
+    {
+
+        LocalDateTime now = LocalDateTime.now();
+        
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        
+        String formattedDateTime = "[" + now.format(formatter) + "]";
+        
+        return formattedDateTime;
+    }
 }
+
+
+
+
