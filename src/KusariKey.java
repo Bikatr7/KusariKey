b50cf7108f064a7e22416e4f6023748643682c1e
Bikatr7 <Tetralon07@gmail.com>
2023-10-22 21:34:48 -0600

    made kusarikey.java nicer

--- src/KusariKey.java
+++ src/KusariKey.java
@@ -2,22 +2,29 @@
 import Modules.FileEnsurer;
 import Handlers.RemoteHandler;
 
+//-------------------start-of-KusariKey()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 public class KusariKey 
 {
-    // Class member declaration
     private static FileEnsurer fileEnsurer;
     private static RemoteHandler remoteHandler;
 
+//-------------------start-of-main()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     public static void main(String[] args) throws Exception 
     {
 
         setup();
     }
 
+//-------------------start-of-setup()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
     private static void setup() throws Exception
     {
         fileEnsurer = new FileEnsurer();
         
+        fileEnsurer.ensureFiles();
+
         remoteHandler = new RemoteHandler();
+
     }
 }
