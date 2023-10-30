75af1127d5558122b6eb99132e8da2b7c6725cde
Bikatr7 <Tetralon07@gmail.com>
2023-10-30 11:49:56 -0600

    added levenshtein

--- src/Modules/toolkit.java
+++ src/Modules/toolkit.java
@@ -179,4 +179,67 @@ public class Toolkit
         System.exit(0);
     }
 
+//-------------------start-of-levenshtein()---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    /**
+     * Calculates the levenshtein distance between two strings.
+     * @param string1 String - the first string
+     * @param string2 String - the second string
+     * @return distance int - the levenshtein distance between the two strings
+     */
+
+    public int levenshtein(String string1, String string2)
+    {
+        int string1Length;
+        int string2Length;
+
+        int[][] distance;
+
+        string1Length = string1.length();
+        string2Length = string2.length();
+
+        // init the distance array, all 0 by default
+        distance = new int[string1Length  + 1][string2Length + 1];
+
+
+        for(int i = 0; i <= string1Length; i++) 
+        {
+            distance[i][0] = i;
+        }
+
+        for(int ii = 0; ii <= string2Length+1; ii++)
+        {
+            distance[0][ii] = ii;
+        }
+
+        for(int i = 1; i < string1Length + 1; i++)
+        {
+            for(int ii = 1; i < string2Length + 1; ii++)
+            {
+                int cost = 0;
+
+                if(string1.charAt(i - 1) == string2.charAt(ii - 1))
+                {
+                    cost = 0;
+                }
+                else
+                {
+                    cost = 1;
+                }
+
+                distance[i][ii] = Math.min(distance[i - 1][ii] + 1, distance[i][ii - 1] + 1);
+
+                distance[i][ii] = Math.min(distance[i][ii], distance[i - 1][ii - 1] + cost);
+
+                if(i > 1 && ii > 1 && string1.charAt(i - 1) == string2.charAt(ii - 2) && string1.charAt(i - 2) == string2.charAt(ii - 1))
+                {
+                    distance[i][ii] = Math.min(distance[i][ii], distance[i - 2][ii - 2] + cost);
+                }
+            }
+        }
+
+        return distance[string1Length][string2Length];
+
+    } 
+
 } // Toolkit
